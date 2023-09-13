4443d16ff64401738b09bf3195370b3ce2153263
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-09-13 18:03:24 +0300

    taxi booking app

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-taxi = {
-    driving: false,
-    location : '',
-    startDriving(){
-        this.driving = true
-    },
-    stopDriving(){
+class Customer {
+    constructor(name, location, destination) {
+        this.name = name
+        this.location = location
+        this.destination = destination
+    }
+    book(taxi) {
+        taxi.navigateTo(this.location)
+        this.enterTaxi(taxi)
+        taxi.navigateTo(this.destination)
+        this.location = taxi.location
+        this.destination = ''
+        this.leaveTaxi(taxi)
+    }
+    enterTaxi(taxi) {
+        taxi.passanger = this
+    }
+    leaveTaxi(taxi) {
+        taxi.passanger = null
+    }
+}
+
+class Taxi {
+    constructor() {
         this.driving = false
-    },
-    navigateTo(destination){
+        this.location = ''
+        this.passanger = null
+    }
+    startDriving() {
+        this.driving = true
+    }
+    stopDriving() {
+        this.driving = false
+    }
+    navigateTo(destination) {
         this.startDriving()
         this.location = destination
         this.stopDriving()
     }
 }
 
-passanger = {
-    name: 'Hakan',
-    location: 'Istanbul',
-    destination: 'Ankara',
-    book(taxi){
-        taxi.navigateTo(this.location) 
-        taxi.navigateTo(this.destination)
-    },
-    enterTaxi(){
+const yellowTaxi = new Taxi()
+const customerEmre = new Customer('Emre', 'Antalya', 'Hatay');
+console.log(yellowTaxi);
 
-    },
-    leaveTaxi(){
 
-    },
+customerEmre.book(yellowTaxi)
 
-}
+console.log(customerEmre, yellowTaxi)
